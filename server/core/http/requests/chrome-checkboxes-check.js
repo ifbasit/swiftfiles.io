b25ea2a795bde72db9238422fb3f892e37b63667
Abdul Basit <ifbasit@gmail.com>
2025-09-03 21:42:48 +0500

    Initial commit

--- server/core/http/requests/chrome-checkboxes-check.js
+++
@@ -1,6 +0,0 @@
-var history_item = document.querySelector("#history-app").shadowRoot.querySelector("#history").shadowRoot.querySelectorAll("history-item");
-
-for(var i = 0; i < history_item.length; i++){
-	let shadow_root = history_item[i].shadowRoot.querySelector("#checkbox").shadowRoot.querySelector("#checkbox").click()
-	console.log(shadow_root)
-}
